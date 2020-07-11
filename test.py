e0be08fb74212e51af86e2fc65af24150834e15b
hillarytapiwatamirepi <46495638+hillarytapiwatamirepi@users.noreply.github.com>
2020-07-11 14:35:32 -0400

    t

--- test.py
+++ test.py
@@ -78,8 +78,8 @@ def main():
     # buying_and_selling_order_every_ten_minute()
     bids[id] = {"type": "add", "order_id": id, "symbol": "BOND", "dir": "BUY", "price":999, "size": 20}
     offers[id] = {"type": "add", "order_id": id, "symbol": "BOND", "dir": "SELL", "price":1001, "size": 20}
-    write_to_exchange(exchange, json.dumps({"type": "add", "order_id": id, "symbol": "BOND", "dir": "BUY", "price":999, "size": 20}))
-    write_to_exchange(exchange,json.dumps({"type": "add", "order_id": id, "symbol": "BOND", "dir": "SELL", "price":1001, "size": 20}))
+    write_to_exchange(exchange, {"type": "add", "order_id": id, "symbol": "BOND", "dir": "BUY", "price":999, "size": 20})
+    write_to_exchange(exchange,{"type": "add", "order_id": id, "symbol": "BOND", "dir": "SELL", "price":1001, "size": 20})
 
     # buy_exchange = read_from_exchange(exchange)
     # print("The exchange replied:", hello_from_exchange, file=sys.stderr)
